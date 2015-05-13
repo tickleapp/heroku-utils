31212d0c0cdad1295172a876b0811c8497fdae58
sodastsai <sodas2002@gmail.com>
2015-05-13 12:25:38 +0800

    Add Apache License 2.0

--- github.py
+++ github.py
@@ -13,11 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-def sns_publish(sns_topic, event, subject, message):
-    if not sns_topic:
-        return
-
-    import boto.sns
-    sns_connection = boto.sns.connect_to_region('us-west-2')
-    sns_connection.publish(topic=sns_topic, subject='[Tickle][{}] {}'.format(event, subject), message=message)
